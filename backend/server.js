bcbb75f910927268ce4e2939aab8fe3d65f353d0
LtARQ <qabdulraouf2@gmail.com>
2024-03-02 21:06:05 +0500

    Deployment build ready

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
@@ -9,6 +10,8 @@ import { app, server } from "./socket/socket.js";
 
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the income request with json payload (from req.body)
@@ -17,8 +20,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.send(`server is running on port ${port} `);
+// app.get("/", (req, res) => {
+//   res.send(`server is running on port ${port} `);
+// });
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(port, () => {
